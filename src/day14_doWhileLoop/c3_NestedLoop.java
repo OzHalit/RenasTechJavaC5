63f56c67da0cf0486ab91e59120989f9bb9c729b
Halit O <halit334@gmail.com>
2022-03-31 21:46:19 -0400

    last update for day 14 java

---
+++ src/day14_doWhileLoop/c3_NestedLoop.java
@@ -0,0 +1,52 @@
+package day14_doWhileLoop;
+
+public class c3_NestedLoop {
+    public static void main(String[] args) {
+
+        //nested if - if inside another if
+        //nested loop - loop inside another loop
+
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Java");
+
+            for (int j = 1; j < 5; j++) {
+                System.out.println("Child Loop");
+
+            }
+
+        }
+        System.out.println("*********************************");
+
+        for (int i = 1; i < 4; i++) {
+            System.out.println("Parent loop start point");
+            for (int k = 3; k < 6; k++) {
+
+                System.out.println("child loop is running i is "+i+" k is "+k);
+
+            }
+            System.out.println("parent loop end");
+
+
+        }
+
+        System.out.println("***********************************************");
+
+        int totalRow2=5;
+        int totalColumn2=30;
+
+        for (int i = 0; i < totalRow2; i++) {
+            int j=1;
+            while (j<=totalColumn2){
+                System.out.print("* ");
+                j++;
+            }
+            System.out.println();
+        }
+
+
+
+
+
+
+    }}
